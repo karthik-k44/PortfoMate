3192c83b368aa950429c86cbfc664c5e6a7df8fd
karthik-k44 <karthikkarunakaran444.com>
2025-03-16 12:03:17 +0530

    Initialize React and Vite project with Tailwind CSS integration and basic structure

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Navbar } from "./Navbar/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+         hai
+    </>
+  )
+}
+
+export default App
